89ee18baa6d0fd5580eba12d63c344aee894e515
Przemysław Kobylański <kobylanski.przemyslaw@gmail.com>
2023-11-25 15:01:58 +0100

    Init Spring Boot Native

---
+++ src/main/java/io/prokobit/springbootnative/SpringBootNativeApplication.java
@@ -0,0 +1,13 @@
+package io.prokobit.springbootnative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/prokobit/springbootnative/SpringBootNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.prokobit.springbootnative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
